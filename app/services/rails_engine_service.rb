5889389e9edb99ecf31c4b6a478cddda2369707b
Steven Stackhouse <stackmm@gmail.com>
2023-07-19 21:18:07 -0600

    add more methods for api services

--- app/services/rails_engine_service.rb
+++ app/services/rails_engine_service.rb
@@ -12,7 +12,19 @@ class RailsEngineService
     get_url("/api/v1/merchants")
   end
 
+  def self.merchant(merchant_id)
+    get_url("/api/v1/merchants/#{merchant_id}")
+  end
+
   def self.merchants_items(merchant_id)
     get_url("/api/v1/merchants/#{merchant_id}/items")
+  end
+
+  def self.all_items
+    get_url("/api/v1/items")
+  end
+
+  def self.item(item_id)
+    get_url("/api/v1/items/#{item_id}")
   end
 end
--- spec/services/rails_engine_service_spec.rb
+++ spec/services/rails_engine_service_spec.rb
@@ -20,6 +20,19 @@ RSpec.describe 'Rails Engine Service' do
     expect(merchants[:data].first[:attributes][:name]).to be_a(String)
   end
 
+  it 'establishes a connection and obtains a single merchant', :vcr do
+    merchant = RailsEngineService.merchant(1)
+
+    expect(merchant).to be_a(Hash)
+    expect(merchant[:data]).to be_a(Hash)
+
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:id]).to be_a(String)
+
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
+  end
+
   it 'establishes a connection and obtains all items for a merchant', :vcr do
     items = RailsEngineService.merchants_items(1)
 
@@ -46,5 +59,61 @@ RSpec.describe 'Rails Engine Service' do
 
     expect(items[:data].first[:attributes]).to have_key(:merchant_id)
     expect(items[:data].first[:attributes][:merchant_id]).to be_an(Integer)
+  end
+
+  it 'establishes a connection and obtains all items', :vcr do
+    items = RailsEngineService.all_items
+
+    expect(items).to be_a(Hash)
+    expect(items[:data]).to be_an(Array)
+
+    expect(items[:data].first).to have_key(:id)
+    expect(items[:data].first[:id]).to be_a(String)
+
+    expect(items[:data].first).to have_key(:type)
+    expect(items[:data].first[:type]).to be_a(String)
+
+    expect(items[:data].first).to have_key(:attributes)
+    expect(items[:data].first[:attributes]).to be_a(Hash)
+
+    expect(items[:data].first[:attributes]).to have_key(:name)
+    expect(items[:data].first[:attributes][:name]).to be_a(String)
+
+    expect(items[:data].first[:attributes]).to have_key(:description)
+    expect(items[:data].first[:attributes][:description]).to be_a(String)
+
+    expect(items[:data].first[:attributes]).to have_key(:unit_price)
+    expect(items[:data].first[:attributes][:unit_price]).to be_a(Float)
+
+    expect(items[:data].first[:attributes]).to have_key(:merchant_id)
+    expect(items[:data].first[:attributes][:merchant_id]).to be_an(Integer)
+  end
+
+  it 'establishes a connection and obtains a single item', :vcr do
+    item = RailsEngineService.item(5)
+
+    expect(item).to be_a(Hash)
+    expect(item[:data]).to be_a(Hash)
+
+    expect(item[:data]).to have_key(:id)
+    expect(item[:data][:id]).to be_a(String)
+
+    expect(item[:data]).to have_key(:type)
+    expect(item[:data][:type]).to be_a(String)
+
+    expect(item[:data]).to have_key(:attributes)
+    expect(item[:data][:attributes]).to be_a(Hash)
+
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to be_a(String)
+    
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes][:description]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+
+    expect(item[:data][:attributes]).to have_key(:merchant_id)
+    expect(item[:data][:attributes][:merchant_id]).to be_an(Integer)
   end
 end

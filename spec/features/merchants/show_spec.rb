2048e53b4dab72b62c014c2ff98533200b493047
Steven Stackhouse <stackmm@gmail.com>
2023-07-19 22:14:54 -0600

    add merchants show page and tests

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -11,9 +11,17 @@ RSpec.describe 'Merchant Show Page' do
     it 'shows a list of items that the merchant sells', :vcr do
       visit merchant_path(1)
 
-      expect(page).to have_content('Item Expedita Aliquam')
-      expect(page).to have_content('Item Provident At')
-      expect(page).to have_content('Item Expedita Fuga')
+      expect(page).to have_link('Item Expedita Aliquam')
+      expect(page).to have_link('Item Provident At')
+      expect(page).to have_link('Item Expedita Fuga')
+    end
+
+    it 'clicking an item name takes me to that items show page', :vcr do
+      visit merchant_path(1)
+
+      click_link('Item Expedita Aliquam')
+
+      expect(current_path).to eq(item_path(5))
     end
   end
 end

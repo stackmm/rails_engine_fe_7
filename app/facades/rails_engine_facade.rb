d3c615e15a2a531d868f12c308996edba9c70e7f
Steven Stackhouse <stackmm@gmail.com>
2023-07-19 21:25:26 -0600

    add additional facades and tests

--- app/facades/rails_engine_facade.rb
+++ app/facades/rails_engine_facade.rb
@@ -7,11 +7,31 @@ class RailsEngineFacade
     end
   end
 
+  def self.merchant(merchant_id)
+    json = RailsEngineService.merchant(merchant_id)
+
+    @merchant = Merchant.new(json[:data])
+  end
+
   def self.merchants_items(merchant_id)
     json = RailsEngineService.merchants_items(merchant_id)
 
     @items = json[:data].map do |item_data|
       Item.new(item_data)
     end
+  end
+
+  def self.all_items
+    json = RailsEngineService.all_items
+
+    @items = json[:data].map do |item_data|
+      Item.new(item_data)
+    end
+  end
+
+  def self.item(item_id)
+    json = RailsEngineService.item(item_id)
+
+    @item = Item.new(json[:data])
   end
 end
--- spec/facades/rails_engine_facade_spec.rb
+++ spec/facades/rails_engine_facade_spec.rb
@@ -10,6 +10,14 @@ RSpec.describe RailsEngineFacade do
     expect(merchants.first.name).to be_a(String)
   end
 
+  it 'returns a single merchant object', :vcr do
+    merchant = RailsEngineFacade.merchant(1)
+
+    expect(merchant).to be_a(Merchant)
+    expect(merchant.id).to be_an(Integer)
+    expect(merchant.name).to be_a(String)
+  end
+
   it 'returns an array of all item objects for a merchant', :vcr do
     items = RailsEngineFacade.merchants_items(1)
 
@@ -20,5 +28,28 @@ RSpec.describe RailsEngineFacade do
     expect(items.first.description).to be_a(String)
     expect(items.first.unit_price).to be_a(Float)
     expect(items.first.merchant_id).to be_an(Integer)
+  end
+
+  it 'returns an array of all item objects', :vcr do
+    items = RailsEngineFacade.all_items
+
+    expect(items).to be_an(Array)
+    expect(items.first).to be_an(Item)
+    expect(items.first.id).to be_an(Integer)
+    expect(items.first.name).to be_a(String)
+    expect(items.first.description).to be_a(String)
+    expect(items.first.unit_price).to be_a(Float)
+    expect(items.first.merchant_id).to be_an(Integer)
+  end
+
+  it 'returns a single item object', :vcr do
+    item = RailsEngineFacade.item(5)
+
+    expect(item).to be_an(Item)
+    expect(item.id).to be_an(Integer)
+    expect(item.name).to be_a(String)
+    expect(item.description).to be_a(String)
+    expect(item.unit_price).to be_a(Float)
+    expect(item.merchant_id).to be_an(Integer)
   end
 end
